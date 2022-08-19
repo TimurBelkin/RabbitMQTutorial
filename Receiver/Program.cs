5349d258c745de0d4dc0684cdb1224cf418bf86c
Timur Belkin <timurbelkin@gmail.com>
2022-08-19 16:00:33 +0300

    RabbitMQ task 2 refactoring

--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -13,38 +13,36 @@ namespace Receiver
         public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() {HostName = "localhost"};
-            using(var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "task_queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
             {
-                channel.QueueDeclare(queue: "task_queue",
-                    durable: true,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received:  {0}", message);
 
+                int dots = message.Split('.').Length - 1;
+                Console.WriteLine(" [x] Sleep {0} sec.", dots);
+                Thread.Sleep(dots * 1000);
+                Console.WriteLine(" [x] Done");
 
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received:  {0}", message);
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
 
-                    int dots = message.Split('.').Length - 1;
-                    Console.WriteLine(" [x] Sleep {0} sec.", dots);
-                    Thread.Sleep(dots * 1000);
-                    Console.WriteLine(" [x] Done");
+            channel.BasicConsume(queue: "task_queue",
+                autoAck: false,
+                consumer: consumer);
 
-                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-                };
-
-                channel.BasicConsume(queue: "task_queue",
-                    autoAck: false,
-                    consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit");
-                Console.ReadLine();
-            }
+            Console.WriteLine(" Press [enter] to exit");
+            Console.ReadLine();
         }
     }
 }
--- Sender/Program.cs
+++ Sender/Program.cs
@@ -2,39 +2,36 @@
 using System.Text;
 using RabbitMQ.Client;
 
-namespace Sender
+namespace Producer
 {
     class Program
     {
         public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() {HostName = "localhost"};
-            using(var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "task_queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            string message = Console.ReadLine();
+            while (!string.IsNullOrEmpty(message))
             {
-                channel.QueueDeclare(queue: "task_queue",
-                    durable: true,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
+                var body = Encoding.UTF8.GetBytes(message);
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
 
-                string message = message = Console.ReadLine();
-                while (!string.Equals(message, ""))
-                {
-                    var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "",
+                    routingKey: "task_queue",
+                    basicProperties: properties,
+                    body: body);
 
-                    var properties = channel.CreateBasicProperties();
-                    properties.Persistent = true;
-
-                    channel.BasicPublish(exchange: "",
-                        routingKey: "task_queue",
-                        basicProperties: properties,
-                        body: body);
-
-                    Console.WriteLine(" [x] Sent {0}", message);
-                    Console.WriteLine(" Enter a word to send and press [enter]. To exit just press [enter]:");
-                    message = Console.ReadLine();
-                }
+                Console.WriteLine(" [x] Sent {0}", message);
+                Console.WriteLine(" Enter a word to send and press [enter]. To exit just press [enter]:");
+                message = Console.ReadLine();
             }
 
             //Console.WriteLine(" Press [enter] to exit.");
