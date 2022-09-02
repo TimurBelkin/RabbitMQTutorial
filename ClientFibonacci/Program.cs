2f17acb23e55ccf3dbd0f5ebef32d222deaf4d82
Timur Belkin <timurbelkin@gmail.com>
2022-09-02 15:40:38 +0300

    Task 6 solved

---
+++ ClientFibonacci/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ClientFibonacci
+{
+    public class RpcClient
+    {
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+        private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+        private readonly IBasicProperties props;
+        private const string HostName = "localhost";
+        private const string RpcQueueName = "rpc_queue";
+
+        public RpcClient()
+        {
+            var factory = new ConnectionFactory() { HostName = HostName};
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new EventingBasicConsumer(channel);
+
+            props = channel.CreateBasicProperties();
+            var correlatedId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlatedId;
+            props.ReplyTo = replyQueueName;
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var response = Encoding.UTF8.GetString(body);
+                if (ea.BasicProperties.CorrelationId == correlatedId)
+                {
+                    respQueue.Add(response);
+                }
+            };
+
+            channel.BasicConsume(
+                consumer: consumer,
+                queue: replyQueueName,
+                autoAck: true
+            );
+        }
+
+        public string Call(string message)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: RpcQueueName,
+                basicProperties: props,
+                body: messageBytes);
+
+            return respQueue.Take();
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+
+    public class Rpc
+    {
+        static void Main(string[] args)
+        {
+            DoFibanacciRequests();
+        }
+
+        private static void DoFibanacciRequests()
+        {
+            var rpcClient = new RpcClient();
+            while (true)
+            {
+                Console.Write("Input a number: ");
+                string number = Console.ReadLine();
+                if (!Int32.TryParse(number, out int _))
+                {
+                    Console.Write(" [x] Failed to parse input number");
+                    break;
+                }
+
+                Console.WriteLine(" [x] Requesting fib({0})", number);
+                var response = rpcClient.Call(number);
+
+                Console.WriteLine(" [.] Got '{0}'", response); 
+            }
+
+            rpcClient.Close();
+        }
+    }
+}
---
+++ ServerFibonacci/Program.cs
@@ -0,0 +1,85 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ServerFibonacci
+{
+    class Program
+    {
+        private const string HostName = "localhost";
+        private const string RpcQueueName = "rpc_queue";
+
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = HostName };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: RpcQueueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queue: RpcQueueName,
+                autoAck: false,
+                consumer: consumer);
+            Console.WriteLine(" [x] Awaiting RPC requests");
+
+            consumer.Received += (model, ea) =>
+            {
+                string response = null;
+                var body = ea.Body.ToArray();
+                var props = ea.BasicProperties;
+                var replyProps = channel.CreateBasicProperties();
+                replyProps.CorrelationId = props.CorrelationId;
+
+                try
+                {
+                    var message = Encoding.UTF8.GetString(body);
+                    int number = int.Parse(message);
+                    Console.WriteLine(" [.] fib({0})", message);
+                    response = Fib(number).ToString();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" [.] " + ex.Message);
+                    response = "";
+                }
+                finally
+                {
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
+                    channel.BasicPublish(exchange: "",
+                        routingKey: props.ReplyTo,
+                        basicProperties: replyProps,
+                        body: responseBytes);
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                        multiple: false);
+                }
+            };
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+            //string message = Console.ReadLine();
+            //while (!string.IsNullOrEmpty(message))
+            //{
+            //    string[] input = message?.Split(' ');
+
+            //    Console.WriteLine(" [x] Sent {0}, routing key: {1}", input[0], input[1]);
+            //    Console.WriteLine(" Enter a word to send and press [enter]. To exit just press [enter]:");
+            //    message = Console.ReadLine();
+            //}
+        }
+
+        private static int Fib(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return number;
+            }
+
+            return Fib(number - 1) + Fib(number - 2);
+        }
+    }
+}
